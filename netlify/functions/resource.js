88c859ee32dcd1ae1f5a7ccbf201e8b61852b4eb
rizz <ganzz808@gmail.com>
2025-10-29 05:05:32 +0700

    update resource

--- netlify/functions/resource.js
+++ netlify/functions/resource.js
@@ -3,20 +3,22 @@ exports.handler = async (event, context) => {
 
   console.log("Function 'resource' triggered, sending 402 response...");
 
-  // 1. Buat objek 'Accepts'
   const paymentMethod = {
     scheme: "exact",
     network: "base",
     maxAmountRequired: "2000000", 
-    resource: `https://\${event.headers.host}\${event.path}`, // Full public URL
-    description: "the hood runs deep in 402.every face got a story. by https://x.com/sanukek https://x402hood.xyz",
+    resource: `https://${event.headers.host}${event.path}`, // Full public URL
+    name: "x402HOOD NFT Mint",
+    description: "the hood runs deep in 402. every face got a story. mint yours at https://x402hood.xyz",
+    website: "https://x402hood.xyz",
+    author: "https://x.com/sanukek",
+    image: "https://i.ibb.co.com/ym2XTpSZ/G4-SIx-Pc-XEAAuo7-O.gif",
+    category: ["NFT", "Crypto", "Base"],
     mimeType: "application/json",
 
-    
     payTo: "0xD95A8764AA0dD4018971DE4Bc2adC09193b8A3c2", 
-
     maxTimeoutSeconds: 600, 
-    asset: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913", 
+    asset: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
 
     outputSchema: {
       input: { type: "http", method: "GET" },
@@ -24,23 +26,20 @@ exports.handler = async (event, context) => {
     }
   };
 
- 
   const x402Response = {
     x402Version: 1,
     error: "Payment Required",
     accepts: [paymentMethod]
   };
 
-
   return {
-        statusCode: 402, 
-        body: JSON.stringify(x402Response), 
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': 'Content-Type',
-          'Access-Control-Allow-Methods': 'GET, OPTIONS'
-        }
-      };
-
+    statusCode: 402, 
+    body: JSON.stringify(x402Response), 
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS'
+    }
+  };
 };
