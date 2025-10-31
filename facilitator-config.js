64965a5e1845833ea06ea25a4c61127a2a75f0c7
rizz <ganzz808@gmail.com>
2025-10-31 19:08:37 +0700

    Move secrets to environment variables

--- facilitator-config.js
+++ facilitator-config.js
@@ -1,8 +1,8 @@
 // facilitator-config.js
 const FACILITATOR_CONFIG = {
-    // Coinbase CDP API Credentials
-    cdpApiKeyId: "81e1ff00-3c6c-44f2-9cf8-0564d1e55307",
-    cdpPrivateKey: "76RTcrYRgRc/Cp2s2giR7oqc0oC8jXKdcgCz3nJ6Gj3Kqi1+Wxbi6XSTVW4SPdBQmMoQRmvXMpehYK2+qhXHaQ==",
+    // Coinbase CDP API Credentials - DARI ENV VARIABLES
+    cdpApiKeyId: process.env.CDP_API_KEY_ID || "",
+    cdpPrivateKey: process.env.CDP_PRIVATE_KEY || "",
     
     // Coinbase CDP Project
     cdpProjectId: "33276cee-1caa-4998-af93-36c5a7f23c52",
@@ -11,7 +11,7 @@ const FACILITATOR_CONFIG = {
     cdpApiUrl: "https://api.developer.coinbase.com",
     cdpRpcUrl: "https://api.developer.coinbase.com/rpc/v1/base",
     
-    // X402 Server
+    // X402 Server (public info - OK to hardcode)
     x402ServerId: "ffb8831e-8ac1-4ed1-bfcb-930bd4ee41f2"
 };
 
